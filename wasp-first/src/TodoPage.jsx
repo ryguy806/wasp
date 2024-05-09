6030254bfe09dffc5a3548d67fe01615a5c06559
Ryan Guelzo <ryan.t.guelzo.ctr@health.mil>
2024-05-09 12:33:14 -0700

    Added query for tasks.

--- wasp-first/src/TodoPage.jsx
+++ wasp-first/src/TodoPage.jsx
@@ -1,3 +1,33 @@
+import { getTasks, useQuery } from "wasp/client/operations";
+
 export const TodoPage = () => {
-  return <div>This is the Todo Page</div>;
+  const { data: tasks, isLoading, error } = useQuery(getTasks);
+
+  return (
+    <div>
+      {tasks && <TaskList tasks={tasks} />}
+      {isLoading && "...Loading"}
+      {error && "Error" + error}
+    </div>
+  );
+};
+
+const taskView = ({ task }) => {
+  return (
+    <div>
+      <input type='checkbox' id={String(task.id)} checked={task.isDOne} />
+      {task.description}
+    </div>
+  );
+};
+
+const TaskList = ({ tasks }) => {
+  if (!tasks?.length) return <div>No tasks</div>;
+  return (
+    <div>
+      {tasks.map(task, (idx) => {
+        <taskView task={task} key={idx} />;
+      })}
+    </div>
+  );
 };
---
+++ wasp-first/src/queries.js
@@ -0,0 +1,5 @@
+export const getTasks = async (args, context) => {
+  return context.entities.Task.findMany({
+    orderBy: { id: "asc" },
+  });
+};
